fc4506114ff487b7d799a26c252f59435ea3b608
Arfa aniq <arfaaniq474@gmail.com>
2024-11-14 04:26:50 +0700

    add searchbar & update manage/list/themes

--- app/src/main/java/com/example/infinitelearning_arfa/component/AboutScreen.kt
+++ app/src/main/java/com/example/infinitelearning_arfa/component/AboutScreen.kt
@@ -42,7 +42,7 @@ fun AboutSreen(navController: NavController) {
         }
 
         Text(
-            text = "Welcome to Anime Handsome Rankings!",
+            text = "Welcome to Anime Biodata and Personality!",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 16.dp)
